24d233ee8a161dbdba9acca0dfdebada11541ff1
Torie <vkim20@gmail.com>
2023-08-24 02:27:48 -0400

    add: five-sort prompt & assumptions

---
+++ 1-array-and-string/five-sort.js
@@ -0,0 +1,26 @@
+/**
+ * Write a function, fiveSort, that takes in an array of numbers
+ * as an argument. The function should rearrange elements of the
+ * array such that all 5s appear at the end. Your function
+ * should perform this operation in-place by mutating the
+ * original array. The function should return the array.
+ *
+ * Elements that are not 5 can appear in any order in the
+ * output, as long as all 5s are at the end of the array.
+ *
+ * EX) fiveSort([12, 5, 1, 5, 12, 7]);
+ *     // -> [12, 7, 1, 12, 5, 5]
+ */
+
+/**
+ * @param {Array} nums -> array of numbers
+ * @returns {Array} nums -> mutated input array
+ *
+ * All elements that are 5 at the end of the array
+ * All other elements can be in any order
+ * In-place mutationß
+ */
+
+const fiveSort = (nums) => {};
+
+export default fiveSort;
