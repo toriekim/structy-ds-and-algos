dc9e505d3d7d11fd354cb611a3a48544d86797c5
Torie <vkim20@gmail.com>
2023-08-24 02:02:20 -0400

    add: pair-product prompt

---
+++ 1-array-and-string/pair-product.js
@@ -0,0 +1,17 @@
+/**
+ * Write a function, pairProduct, that takes in an array and a
+ * target product as arguments. The function should return an
+ * array containing a pair of indices whose elements multiply
+ * to the given target. The indices returned must be unique.
+ *
+ * Be sure to return the indices, not the elements themselves.
+ *
+ * There is guaranteed to be one such pair whose product is the
+ * target.
+ *
+ * EX) pairProduct([3, 2, 5, 4, 1], 8); // -> [1, 3]
+ */
+
+const pairProduct = (numbers, targetProduct) => {};
+
+export default pairProduct;
