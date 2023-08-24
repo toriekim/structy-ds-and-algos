ea4c1ce6307ffa1291dcdc51a7b4db336203935e
Torie <vkim20@gmail.com>
2023-08-24 02:22:24 -0400

    add: intersection approach & solution

--- 1-array-and-string/intersection.js
+++ 1-array-and-string/intersection.js
@@ -13,8 +13,29 @@
  * @param {Array} a -> array of unique numbers
  * @param {Array} b -> array of unique numbers
  * @returns {Array} -> new array of elements that are in both
+ *
+ * No duplicate elements in each input array
+ * The arrays may or may not be in some order
+ * The arrays may not be the same length
+ * It's possible that there are no intersections
+ *
+ * Approach:
+ * Create Set from shorter array
+ * Initialize results array
+ * Loop through longer array and check if it's in the set
+ *   If it is, add to results array
+ * Return results array
  */
 
-const intersection = (a, b) => {};
+const intersection = (a, b) => {
+  const setA = new Set(a);
+  const results = [];
+
+  for (let num of b) {
+    if (setA.has(num)) results.push(num);
+  }
+
+  return results;
+};
 
 export default intersection;
