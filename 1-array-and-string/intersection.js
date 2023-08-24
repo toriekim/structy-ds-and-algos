f63c3c47e482a30dcd792ebd91b6be63df8bcb0c
Torie <vkim20@gmail.com>
2023-08-24 02:13:43 -0400

    add: intersection prompt

---
+++ 1-array-and-string/intersection.js
@@ -0,0 +1,20 @@
+/**
+ * Write a function, intersection, that takes in two arrays,
+ * a,b, as arguments. The function should return a new array
+ * containing elements that are in both of the two arrays.
+ *
+ * You may assume that each input array does not contain
+ * duplicate elements.
+ *
+ * EX) intersection([4,2,1,6], [3,6,9,2,10]) // -> [2,6]
+ */
+
+/**
+ * @param {Array} a -> array of unique numbers
+ * @param {Array} b -> array of unique numbers
+ * @returns {Array} -> new array of elements that are in both
+ */
+
+const intersection = (a, b) => {};
+
+export default intersection;
