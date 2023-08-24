d29777869e25db85132149b58fdd9f3f9fc84ecb
Torie <vkim20@gmail.com>
2023-08-24 02:07:31 -0400

    add: pair-product approach

--- 1-array-and-string/pair-product.js
+++ 1-array-and-string/pair-product.js
@@ -12,6 +12,24 @@
  * EX) pairProduct([3, 2, 5, 4, 1], 8); // -> [1, 3]
  */
 
+/**
+ * @param {Array} numbers -> array of numbers
+ * @param {number} targetProduct -> resulting product of 2 numbers
+ * @returns {Array} -> array of indices of the 2 numbers
+ *
+ * Guaranteed to be one such pair whose product is the target
+ * Shouldn't sort input array b/c we need indices
+ *
+ * Any negative numbers? No
+ *
+ * Approach:
+ * Initialize map for keeping track of possible pairs
+ * Loop through numbers array
+ * Calculate pair = targetProduct / numbers[i]
+ * If pair is in map, return map[pair], i
+ * Else add numbers[i], i to map
+ */
+
 const pairProduct = (numbers, targetProduct) => {};
 
 export default pairProduct;
