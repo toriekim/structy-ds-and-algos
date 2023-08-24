60f50cb8932ce4e72ee6c1d6ac906b4491e1a031
Torie <vkim20@gmail.com>
2023-08-24 02:38:55 -0400

    add: fiveSort approach, complexity, solution

--- 1-array-and-string/five-sort.js
+++ 1-array-and-string/five-sort.js
@@ -18,9 +18,36 @@
  *
  * All elements that are 5 at the end of the array
  * All other elements can be in any order
- * In-place mutationß
+ * In-place mutations
+ *
+ * Approach:
+ * 2 pointers -> i: start front, j: starts back
+ * while loop i < j
+ * check if nums[i] === 5
+ *   if it is 5, swap with nums[j] & j--, i++
+ *   else, i++
+ * return nums
  */
 
-const fiveSort = (nums) => {};
+// n = array size (length of array)
+// Time Complexity: O(n)
+// Space Complexity: O(1) -> mutating input array
+
+const fiveSort = (nums) => {
+  let i = 0;
+  let j = nums.length - 1;
+  while (i < j) {
+    if (nums[j] === 5) {
+      j--;
+    } else if (nums[i] === 5) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      j--;
+      i++;
+    } else {
+      i++;
+    }
+  }
+  return nums;
+};
 
 export default fiveSort;
